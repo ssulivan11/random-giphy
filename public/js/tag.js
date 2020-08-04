de7809f40881b247e1cbefc0b6504e61ef35fdb7
Scott Sullivan <ssulivan11@gmail.com>
2020-08-04 14:40:23 -0500

    stage 4

--- public/js/tag.js
+++ public/js/tag.js
@@ -1,28 +1,20 @@
-'use strict'
-exports.__esModule = true
-var Tag = function(html) {
-  var name = 'tag'
-  var value = html.value
-  html.addEventListener('keyup', function(event) {
-    return changeTo(event.target.value)
-  })
-  var changeTo = function(newValue) {
-    updateValue(newValue)
-    render()
-  }
-  var getValue = function() {
-    return value
-  }
-  var updateValue = function(newValue) {
-    return (value = newValue)
-  }
-  var render = function() {
-    return (html.value = value)
-  }
-  return {
-    name: name,
-    changeTo: changeTo,
-    getValue: getValue
-  }
-}
-exports['default'] = Tag
+"use strict";
+exports.__esModule = true;
+var Tag = function (html) {
+    var name = 'tag';
+    var value = html.value;
+    html.addEventListener('keyup', function (event) { return changeTo(event.target.value); });
+    var changeTo = function (newValue) {
+        updateValue(newValue);
+        render();
+    };
+    var getValue = function () { return value; };
+    var updateValue = function (newValue) { return (value = newValue); };
+    var render = function () { return (html.value = value); };
+    return {
+        name: name,
+        changeTo: changeTo,
+        getValue: getValue
+    };
+};
+exports["default"] = Tag;
